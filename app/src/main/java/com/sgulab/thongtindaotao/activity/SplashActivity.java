906e9067c8abfb5d432861856b1d98d5419f2929
ToanTruong <toan.truong@dirox.net>
2016-04-01 18:18:42 +0700

    add basic login screen

--- app/src/main/java/com/sgulab/thongtindaotao/activity/SplashActivity.java
+++ app/src/main/java/com/sgulab/thongtindaotao/activity/SplashActivity.java
@@ -1,15 +1,39 @@
 package com.sgulab.thongtindaotao.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.sgulab.thongtindaotao.R;
 
 public class SplashActivity extends BaseActivity {
 
+  private Button btLogin;
+  private Button btGuest;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
+
+    btLogin = (Button) findViewById(R.id.button_splash_account);
+    btGuest = (Button) findViewById(R.id.button_splash_guest);
+
+    btLogin.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+      }
+    });
+
+    btGuest.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+      }
+    });
   }
 }
